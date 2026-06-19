49e445f7e796eadab16953eebbb715a68e56363d
Saumit Paul <48136910+5aumit@users.noreply.github.com>
2026-06-18 21:19:48 -0400

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        ps = dict(zip(position,speed))
+        position = sorted(position)
+
+        timeleft = [(target-p)/ps[p] for p in position]
+
+        fleets = []
+
+        for i,t in enumerate(timeleft):
+            
+            while fleets and t>fleets[-1]:
+                fleets.pop()
+
+            fleets.append(t)
+
+            # print(fleets)
+
+        return len(set(fleets))
+
+
+
+
+        
